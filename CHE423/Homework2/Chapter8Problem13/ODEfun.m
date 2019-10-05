6e7c3e9bbe73b20c6131917627e5ecfab41e5714
j-hayes <jhayes2118@gmail.com>
2019-10-04 22:44:03 -0500

    add another problem

--- CHE423/Homework2/Chapter8Problem13/ODEfun.m
+++ CHE423/Homework2/Chapter8Problem13/ODEfun.m
@@ -1,15 +1,50 @@
-function dYfuncvecdt = ODEfun(t,Yfuncvec,k)
-Ca = Yfuncvec(1); 
-Cr = Yfuncvec(2); 
-Cs = Yfuncvec(3); 
+function dYfuncvecdt = ODEfun(t,Yfuncvec,params)
+Fa = Yfuncvec(1); 
+Fb = Yfuncvec(2); 
+Fc = Yfuncvec(3); 
+Fd = Yfuncvec(4); 
+Fe = Yfuncvec(5); 
+y = Yfuncvec(6); 
+
 % Explicit equations
-k1=k(1);
-k2=k(2);
-k3 = k(3);
+
+k1c=params(1);
+k2d= params(2);
+k3e = params(3);
+Wf = params(4);
+alpha = params(5);
+K1c = params(6);
+kc = params(7);
+Ct0 = params(8);
+kb = params(9);
+Ft0 = params(10);
+
+Ft = Fa + Fb + Fc + Fd + Fe;
+
+Ca = Ct0*(Fa/Ft)*y;
+Cb = Ct0*(Fb/Ft)*y;
+Cc = Ct0*(Fc/Ft)*y;
+Cd = Ct0*(Fd/Ft)*y;
+Ce = Ct0*(Fe/Ft)*y;
+
+r2d = k2d*Ca;
+r1c = k1c*(Ca-(Cb*Cc/K1c));
+r3e = k3e*(Cc^2)*Cd;
+
+ra = 0-r1c-r2d;
+rb = r1c;
+rc=r1c-r3e;
+rd = r2d-(r3e/2);
+re = r3e;
+
 
 % Differential equations
-dCadt = -k1*Ca; 
-dCrdt = k1*Ca-k2*Cr-k3*Cr*Cs; 
-dCsdt = k2*Cr - k3*Cr*Cs; 
-dYfuncvecdt = [dCadt; dCrdt; dCsdt]; 
+dFadW = ra;
+dFbdW=  rb;
+dFcdW = rc -(kb*Cc);
+dFddW = rd;
+dFedW = re;
+dydW = -alpha*Ft/(2*Ft0*y);
+
+dYfuncvecdt = [dFadW; dFbdW; dFcdW;dFddW;dFedW;dydW]; 
 
