e9ad9be3ec75164feac01dd3a5626f306a4fe28e
j-hayes <jhayes2118@gmail.com>
2019-10-16 19:29:08 -0500

    finish g&R 1.21

--- CHE535/hw5/GramRChapter1Problem22.m
+++ CHE535/hw5/GramRChapter1Problem22.m
@@ -1,20 +1,21 @@
 experimentalTValues = transpose([300 325 350 375 400 425 450 475 500]);
 experimentalkValues = transpose([1.82 1.89 2.02 2.14 2.12 2.17 2.15 2.21 2.26]);
+
+%part a) setup model
 logExperimentalKValues = log(experimentalkValues);
-PHI = zeros(10,1);
-
-
 y_aproximated_forPlot =  zeros(1,plot_x_count);
 y_aproximated = zeros(10,1);
 A = ones(9, 2);
 A(:,2) = -1./experimentalTValues;
-
+%part b) calculate least squares values of x 
 X_ls = inv(transpose(A)*A) * transpose(A)*logExperimentalKValues;
 
+%part c) is the solution unique ie is nullspace of a empty 
+nullSpace = null(A);
+
+%part d plot
 b_approximated = A*X_ls;
 k_approximated = exp(b_approximated);
-
-errors = (experimentalkValues - k_approximated)./experimentalkValues;
 
 plot(experimentalTValues,experimentalkValues,'-k*',experimentalTValues, k_approximated,'-k')
 title('Least Squares')
@@ -22,3 +23,7 @@ legend('Expiremental Data', 'Least Squares aproximation');
 xlabel('T')
 ylabel('k')
 saveas(gcf,'Least_Squares_arrhenius.png')
+
+% is it a good fit?
+errors = (experimentalkValues - k_approximated)./experimentalkValues;
+
